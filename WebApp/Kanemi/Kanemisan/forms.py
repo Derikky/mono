737d491137c62b1b37bae2bc2c72539e86acedc9
Derikky <132252551+Derikky@users.noreply.github.com>
2023-10-05 16:17:05 +0900

    ボタン増加

--- WebApp/Kanemi/Kanemisan/forms.py
+++ WebApp/Kanemi/Kanemisan/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import ModelForm, BaseModelFormSet, modelformset_factory
 from .models import Things
 
 
@@ -9,3 +10,11 @@ class ThingsForm(forms.ModelForm):
         labels = {'name':'商品名', 'price': '単品価格', 'amount':'数量'}
         
     
+#class BaseThingsFormSet(BaseModelFormSet):
+#    def __init__(self, *args, **kwargs):
+#        super().__init__(*args, **kwargs)
+#        # 初期値を設定する
+#        self.queryset = Memo.objects.filter(title__startswith=title)
+
+#ThingsFormSet = modelformset_factory(Things, Form=ThingsForm, formset=BaseThingsFormSet)
+
